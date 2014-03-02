2cac83eb5ebf66090fc76ee826f373846ae1de15
metalkettle <james.redfern@gmail.com>
2014-03-02 16:39:12 +0000

    Themes Code improvements

--- plugin.audio.televisiontunes/default.py
+++ plugin.audio.televisiontunes/default.py
@@ -1,5 +1,6 @@
 import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmc,xbmcaddon,os
 from resources.libs.common_addon import Addon
+from metahandler import metahandlers
 
 addon_id = 'plugin.audio.televisiontunes'
 addon = Addon('plugin.audio.televisiontunes', sys.argv)
@@ -10,6 +11,7 @@ afb = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'afb.
 console = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'console.png'))
 tv = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'tv.png'))
 football = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'football.png'))
+metaget = metahandlers.MetaData(preparezip=False)
 
 def index():
         addDir('Television Tunes','http://www.televisiontunes.com/',1,tv,'',fanart)
@@ -25,53 +27,38 @@ def AZ(url):
         if 'fight' in url : img = afb
         if 'fc' in url : img = football
         base = url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+        link = open_url(url)
         azlist=re.compile('<li><a href="(.+?)" >(.+?)</a></li>').findall(link)
         addDir('Search',url,3,img,'',fanart)
         for url,name in azlist:
                 addDir(name,base + url,2,img,'',fanart)
                
-def GETTHEMES(url):
+def GETTHEMES(url,name):
         if 'television' in url : img = tv
         if 'game' in url : img = console
         if 'tvad' in url : img = ad
         if 'fight' in url : img = afb
         if 'fc' in url : img = football
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+        link = open_url(url)
         match=re.compile('<a href="(.+?)">(.+?)</a></td></tr><tr><td>').findall(link)
         for url,name in match:
-                addDir(name,url,100,img,'',fanart)
+                addDirPlayable(name,url,100,img,'',fanart)
                 
 def afblist(url):
         base = url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+        link = open_url(url)
         catlist=re.compile('<a href="(.+?)"><img src="(.+?)" alt="(.+?)" width="150" height="100" border="0" /></a></td>').findall(link)
         for url,thumb,name in catlist:
                 thumb2 = base + thumb
                 addDir(name,url,2,thumb2,'',fanart)
         
-def getafbthemes(url):
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
+def getafbthemes(url,name):
+        link = open_url(url)
         response.close()
         match=re.compile('<a href="(.+?)">(.+?)</a></td></tr>').findall(link)
         for url,name in match:
-                addDir(name,url,100,icon,'',fanart)
-                
+                addDirPlayable(name,url,100,icon,'',fanart)
+        
 def SEARCH(url):
     base = url
     search_entered =''
@@ -79,34 +66,31 @@ def SEARCH(url):
     keyboard.doModal()
     if keyboard.isConfirmed():
         search_entered = keyboard.getText().replace(' ','+')# sometimes you need to replace spaces with + or %20#
-    if search_entered == None or len(search_entered)<1:
-        end()
-    else:
+    if len(search_entered)>1:
         url = base + 'search.php?searWords=' + search_entered + '&Send=Search'
         print url
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+        link = open_url(url)
         match=re.compile('&nbsp;<a href="(.+?)">(.+?)</a>').findall(link)
         for url,name in match:
-                addDir(name,url,100,icon,'',fanart) 
+                addDirPlayable(name,url,100,icon,'',fanart)
+                
     
 def TUNELINKS(url,name):
+        print name
         base ='http://' + url.split('/',3)[2]
-        req = urllib2.Request(url)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
-        response = urllib2.urlopen(req)
-        link=response.read()
-        response.close()
+        link = open_url(url)
         tune=re.compile('mp3: "(.+?)"').findall(link)
         for url in tune:
                 fulllink = base + url
                 nospace = str(fulllink).replace(' ','%20')
-                xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-                xbmcPlayer.play(nospace)
-                exit()
+        playlist = xbmc.PlayList(1)
+        playlist.clear()
+        liz=xbmcgui.ListItem(name, iconImage="DefaultVideo.png", thumbnailImage='')
+        liz.setInfo('music', {'Title':name})
+        liz.setProperty('mimetype', 'audio/mpeg')                
+        playlist.add(nospace, liz)
+        xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
+        xbmcPlayer.play(playlist)
  
 def get_params():
         param=[]
@@ -125,16 +109,48 @@ def get_params():
                                 param[splitparams[0]]=splitparams[1]
                                
         return param
+
+def open_url(url):
+        req = urllib2.Request(url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        return link
                
 def addDir(name,url,mode,iconimage,description,fanart):
         u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        print name
         ok=True
         liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
-        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
         liz.setProperty('fanart_image', fanart)
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
         return ok
- 
+
+def addDirPlayable(name,url,mode,iconimage,description,fanart):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        print name
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+
+def addDirMeta(name,url,mode,iconimage,itemcount,isFolder=True):
+        meta = metaget.get_meta('tv show', name ,year='')
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&site="+str(site)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels= meta )
+        contextMenuItems = []
+        contextMenuItems.append(('Movie Information', 'XBMC.Action(Info)'))
+        liz.addContextMenuItems(contextMenuItems, replaceItems=True)
+        if not meta['backdrop_url'] == '': liz.setProperty('fanart_image', meta['backdrop_url'])
+        else: liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=isFolder,totalItems=itemcount)
+        xbmc.executebuiltin('Container.SetViewMode(500)')
+        return ok
+
 params=get_params(); url=None; name=None; mode=None; site=None
 try: site=urllib.unquote_plus(params["site"])
 except: pass
@@ -149,7 +165,7 @@ print "Site: "+str(site); print "Mode: "+str(mode); print "URL: "+str(url); prin
  
 if mode==None or url==None or len(url)<1: index()
 elif mode==1: AZ(url)
-elif mode==2: GETTHEMES(url)
+elif mode==2: GETTHEMES(url,name)
 elif mode==3: SEARCH(url)
 elif mode==4: afblist(url)
 elif mode==100: TUNELINKS(url,name)
