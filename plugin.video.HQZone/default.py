c9dfa54592497802db86ebc00a1d32a2f18f5ef1
metalkettle <james.redfern@gmail.com>
2014-11-18 20:39:20 +0000

    HQZone 1.0.6

--- plugin.video.HQZone/default.py
+++ plugin.video.HQZone/default.py
@@ -2,18 +2,15 @@ import xbmc, xbmcgui, xbmcaddon, xbmcplugin, urllib, re, string, os, time
 from t0mm0.common.net import Net as net
 
 addon_id 	= 'plugin.video.HQZone'
-local 		= xbmcaddon.Addon(id=addon_id)
 art 		= xbmc.translatePath(os.path.join('special://home/addons/' + addon_id + '/resources/art/'))
 selfAddon 	= xbmcaddon.Addon(id=addon_id)
 user 		= selfAddon.getSetting('hqusername')
 passw 		= selfAddon.getSetting('hqpassword')
 datapath 	= xbmc.translatePath(selfAddon.getAddonInfo('profile'))
-cookie_file = os.path.join(os.path.join(datapath,'Cookies'), 'hqzone.cookies')
-Dir 		= xbmc.translatePath(os.path.join('special://home/addons/plugin.video.HQZone', ''))
-fanartimage	= Dir+'fanart.jpg'
-icon = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
+cookie_file     = os.path.join(os.path.join(datapath,'Cookies'), 'hqzone.cookies')
+fanart          = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id , 'fanart.jpg'))
+icon            = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
 
-#set paths
 CachePath=os.path.join(datapath,'Cache')
 try: os.makedirs(CachePath)
 except: pass
@@ -30,7 +27,6 @@ def setCookie(srDomain):
     m.update(passw)
     net().http_GET('http://www.hqzone.tv/forums/view.php?pg=live')
     net().http_POST('http://www.hqzone.tv/forums/login.php?do=login',{'vb_login_username':user,'vb_login_password':passw,'vb_login_md5password':m.hexdigest(),'vb_login_md5password_utf':m.hexdigest(),'do':'login','securitytoken':'guest','url':'http://www.hqzone.tv/forums/view.php?pg=live','s':''})
-
 if user == '' or passw == '':
     if os.path.exists(cookie_file):
         try: os.remove(cookie_file)
@@ -41,101 +37,96 @@ if user == '' or passw == '':
         keyb = xbmc.Keyboard('', 'Enter Username or Email')
         keyb.doModal()
         if (keyb.isConfirmed()):
-            search = keyb.getText()
-            username=search
+            username = keyb.getText()
             keyb = xbmc.Keyboard('', 'Enter Password:')
             keyb.doModal()
             if (keyb.isConfirmed()):
-                search = keyb.getText()
-                password=search
+                password = keyb.getText()
                 selfAddon.setSetting('hqusername',username)
                 selfAddon.setSetting('hqpassword',password)
-#		setCookie('http://www.hqzone.tv/forums/view.php?pg=live')
 
-def addDir(name,url,mode,iconimage,plot='',fanart='',index=False):
-    return addDirX(name,url,mode,iconimage,plot,fanart,addToFavs=0,replaceItems=False,index=index)
+def addDir(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
 
-def addLink(name,url,iconimage):
-    liz=xbmcgui.ListItem(name, iconImage=art+'/link.png', thumbnailImage=iconimage)
-    liz.setInfo( type="Video", infoLabels={ "Title": name } )
-    liz.setProperty('fanart_image', fanartimage)
-    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)	
-
-def addPlayL(name,url,mode,iconimage,plot,fanart,dur,genre,year,secName='',secIcon=''):
-    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&plot="+urllib.quote_plus(plot)+"&fanart="+urllib.quote_plus(fanart)+"&genre="+urllib.quote_plus(genre)
-    surl=urllib.quote_plus(u)
-    dname=removeColoredText(name)
-    mi=[('Add to [COLOR=FFa11d21][B]ONTapp.tv[/B][/COLOR]', 'XBMC.RunPlugin(%s?mode=1501&plot=%s&name=%s&url=%s&iconimage=%s)' % (sys.argv[0] ,secName,dname,surl, secIcon))]
-    return addDirX(name,url,mode,iconimage,plot,fanart,dur,genre,year,isFolder=0)
-
-def VIEWSB():
-    if selfAddon.getSetting("auto-view") == "true":
-        if selfAddon.getSetting("home-view") == "0":
-                xbmc.executebuiltin("Container.SetViewMode(50)")
-        elif selfAddon.getSetting("home-view") == "1":
-                xbmc.executebuiltin("Container.SetViewMode(50)")
-        return
-
-def removeColoredText(text):
-    return re.sub('\[COLOR.*?\[/COLOR\]','',text,re.I|re.DOTALL).strip()
+def addLink(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
 	
-def MAIN():
+def MainMenu():
     setCookie('http://www.hqzone.tv/forums/view.php?pg=live')
     response = net().http_GET('http://www.hqzone.tv/forums/view.php?pg=live')
     link = response.content
     link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','').replace('  ','')
-    addDir('[COLOR cyan]View Schedule[/COLOR]','http://www.hqzone.tv/forums/calendar.php?c=1&do=displayweek',6,icon)
-    addLink('_________________________','',icon)
+    addDir('[COLOR white][B]-- View Schedule --[/B][/COLOR]','http://www.hqzone.tv/forums/calendar.php?c=1&do=displayweek',6,icon,fanart)
+    addLink('[COLOR blue]_________________________[/COLOR]','','',icon,fanart)
     match=re.findall('(?sim)<h4 class="panel_headin.+?">([^<]+?)</h4><ul>(.+?)</ul>',link)
     for name,links in match[0:3]:
         if 'Channels' == name:
-            name='V.I.P Member Streams'
-        addDir(name,links,2,icon) #Main Channels
-    addLink('_________________________','',icon)
+            name='[COLOR gold]VIP[/COLOR]'+' Member Streams'
+        addDir(name,links,2,icon,fanart) #Main Channels
+    addLink('[COLOR blue]_________________________[/COLOR]','','',icon,fanart)
     match=re.findall('(?sim)<h4 class="panel_headin.+?">([^<]+?)</h4><ul>(.+?)</ul>',link)
     for name,links in match[3:]:
         if 'Channels' == name:
-            name='V.I.P Member Streams'
-        addDir(name,links,4,icon) #VIP
+            name='[COLOR gold][B}VIP[/B][/COLOR]'+' Member Streams'
+        addDir(name,links,4,icon,fanart) #VIP
+    addLink('','','',icon,fanart)
+    addLink('[COLOR red][I]** NOTE:  If a stream fails to play, the selected channel is likely offline **[/I][/COLOR]','','',icon,fanart)
+   
  
-def LISTMENU(murl):
-    match=re.findall('(?sim)<li><a href="([^"]+?)" target="I1">([^<]+?)</a></li>',murl)
+def VIPMenu(url):
+    match=re.findall('(?sim)<li><a href="([^"]+?)" target="I1">([^<]+?)</a></li>',url)
     if not match:
-        match=re.findall('(?sim)<a href="([^"]+?)" target="I1"><img src="([^"]+?)"',murl)
+        match=re.findall('(?sim)<a href="([^"]+?)" target="I1"><img src="([^"]+?)"',url)
     for url,name in match:
         url = 'http://www.hqzone.tv/forums/'+url
-        addPlayL(name,url,5,'','','','','','')
- 
- #Display the VOD TV section
-def LISTMENU2(murl):
-    match=re.findall('(?sim)<li><a href="([^"]+?)" target="I1">([^<]+?)</a></li>',murl)
+        addLink(name,url,5,icon,fanart)
+
+def VODMenu(url):
+    match=re.findall('(?sim)<li><a href="([^"]+?)" target="I1">([^<]+?)</a></li>',url)
     for url,name in match:
         url = 'http://www.hqzone.tv/forums/'+url
-        addDir(name,url,3,icon) #default image for VOD section
+        addDir(name,url,3,icon,fanart) #default image for VOD section
  
-def LISTCONTENT(murl,thumb):
-    setCookie(murl)
-    response = net().http_GET(murl)
+def GetLinks(url,thumb):
+    setCookie(url)
+    response = net().http_GET(url)
     link = response.content
     link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','').replace('  ','')
     match=re.findall('(?sim)sources: \[\{ file: "([^"]+?)" \}\],title: "([^"]+?)"',link)
     for url,name in match:
-        addPlayL(name,url,5,'','','','','','')
+        addLink(name,url,5,icon,fanart)
 		
-def PLAYLINK(mname,murl,thumb):
+def PlayStream(name,url,thumb):
+    notification('HQ Zone', 'Requesting stream from server', '3000',icon)
+    try:
         ok=True
-        url = get_link(murl)     
+        url = get_link(url)     
         liz=xbmcgui.ListItem(name, iconImage=icon,thumbnailImage=icon); liz.setInfo( type="Video", infoLabels={ "Title": name } )
         ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
         xbmc.Player ().play(url, liz, False)
+    except:
+        print 'FAIL'
+        pass
         
-def get_link(murl):
-    if 'mp4' in murl:
+def get_link(url):
+    if 'mp4' in url:
         swf='http://www.hqzone.tv/forums/jwplayer/jwplayer.flash.swf'
-        streamer=re.search('(?sim)(rtmp://.+?/vod/)(.+?.mp4)',murl)
+        streamer=re.search('(?sim)(rtmp://.+?/vod/)(.+?.mp4)',url)
         return streamer.group(1)+'mp4:'+streamer.group(2)+' swfUrl='+swf+' pageUrl=http://www.hqzone.tv/forums/view.php?pg=live# token=WY846p1E1g15W7s'
-    setCookie(murl)
-    response = net().http_GET(murl)
+    setCookie(url)
+    response = net().http_GET(url)
     link = response.content
     link = cleanHex(link)
     link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','').replace('  ','')
@@ -145,27 +136,26 @@ def get_link(murl):
         return m3u8[0]
     elif flash:
         swf='http://www.hqzone.tv/forums/jwplayer/player.swf'
-        return flash.group(2)+' playpath='+flash.group(1)+' swfUrl='+swf+' pageUrl='+murl+' live=true timeout=20 token=WY846p1E1g15W7s'
-
+        return flash.group(2)+' playpath='+flash.group(1)+' swfUrl='+swf+' pageUrl='+url+' live=true timeout=20 token=WY846p1E1g15W7s'
     else:
         swf='http://www.hqzone.tv/forums/jwplayer/jwplayer.flash.swf'
         streamer=re.findall("file: '([^']+)',",link)[0]
-        return streamer.replace('redirect','live')+' swfUrl='+swf+' pageUrl='+murl+' live=true timeout=20 token=WY846p1E1g15W7s'
+        return streamer.replace('redirect','live')+' swfUrl='+swf+' pageUrl='+url+' live=true timeout=20 token=WY846p1E1g15W7s'
 
-def Calendar(murl):
-    setCookie(murl)
-    response = net().http_GET(murl)
+def Schedule(url):
+    setCookie(url)
+    response = net().http_GET(url)
     link = response.content
     link=link.replace('\r','').replace('\n','').replace('\t','').replace('&nbsp;','').replace('  ','')
     month=re.findall('(?sim)<h2 class="blockhead">([^<]+?)</h2>',link)
     match=re.findall('(?sim)<h3><span class=".+?">([^<]+?)</span><span class="daynum" style=".+?" onclick=".+?">(\d+)</span></h3><ul class="blockrow eventlist">(.+?)</ul>',link)
     for day,num,data in match:
-		addLink('[COLOR yellow]Times are E.S.T / GMT -5 | Follow us on Twitter for latest channel news, updates + more.[/COLOR]','','') 
-		addLink('[COLOR blue]'+day+' '+num+' '+month[0]+'[/COLOR]','','')
+		addLink('[COLOR yellow]Times are E.S.T / GMT -5 | Follow us on Twitter for latest channel news, updates + more.[/COLOR]','','',icon,fanart) 
+		addLink('[COLOR blue]'+day+' '+num+' '+month[0]+'[/COLOR]','','',icon,fanart)
 		match2=re.findall('(?sim)<span class="eventtime">([^<]+?)</span><a href=".+?" title=".+?">([^<]+?)</a>',data)
 		for time,title in match2:
-			addLink('[COLOR yellow]'+time+'[/COLOR] '+title,'','')
-
+			addLink('[COLOR yellow]'+time+'[/COLOR] '+title,'','',icon,fanart)
+			
 def cleanHex(text):
     def fixup(m):
         text = m.group(0)
@@ -173,21 +163,9 @@ def cleanHex(text):
         else: return unichr(int(text[2:-1])).encode('utf-8')
     return re.sub("(?i)&#\w+;", fixup, text.decode('ISO-8859-1').encode('utf-8'))
 
-def addDirX(name,url,mode,iconimage,plot='',fanart='',dur=0,genre='',year='',imdb='',tmdb='',isFolder=True,searchMeta=False,addToFavs=True,
-            id=None,fav_t='',fav_addon_t='',fav_sub_t='',metaType='Movies',menuItemPos=None,menuItems=None,down=False,replaceItems=True,index=False):
-    u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&iconimage="+urllib.quote_plus(iconimage)+"&plot="+urllib.quote_plus(plot)+"&fanart="+urllib.quote_plus(fanart)+"&genre="+urllib.quote_plus(genre)+"&index="+str(index)
-    if not fanart: fanart=fanartimage
-    if not iconimage: iconimage=icon
-    Commands = []
-  
-    
-    liz=xbmcgui.ListItem(name, iconImage=art+'/vidicon.png', thumbnailImage=iconimage)
-    liz.addContextMenuItems( Commands, replaceItems=False)
-    if searchMeta:
-        liz.setInfo( type="Video", infoLabels=infoLabels )
-    liz.setProperty('fanart_image', fanart)
-    return xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=isFolder)
-	
+def notification(title, message, ms, nart):
+    xbmc.executebuiltin("XBMC.notification(" + title + "," + message + "," + ms + "," + nart + ")")
+
 def get_params():
     param=[]
     paramstring=sys.argv[2]
@@ -205,75 +183,27 @@ def get_params():
                 param[splitparams[0]]=splitparams[1]
     return param
               
-params=get_params()
-
-url=None
-name=None
-mode=None
-iconimage=None
-fanart=None
-plot=None
-genre=None
-title=None
-season=None
-episode=None
-location=None
-path=None
-index=None
-
+params=get_params(); url=None; name=None; mode=None; path=None; iconimage=None
 try: name=urllib.unquote_plus(params["name"])
 except: pass
 try: url=urllib.unquote_plus(params["url"])
 except: pass
 try: mode=int(params["mode"])
 except: pass
-try:
-    iconimage=urllib.unquote_plus(params["iconimage"])
-    iconimage = iconimage.replace(' ','%20')
+try:iconimage=urllib.unquote_plus(params["iconimage"])
 except: pass
 try: plot=urllib.unquote_plus(params["plot"])
 except: pass
-try:
-    fanart=urllib.unquote_plus(params["fanart"])
-    fanart = fanart.replace(' ','%20')
-except: pass
-try: genre=urllib.unquote_plus(params["genre"])
-except: pass
 try: title=urllib.unquote_plus(params["title"])
-except: pass
-try: episode=int(params["episode"])
-except: pass
-try: season=int(params["season"])
-except: pass
-try: location=urllib.unquote_plus(params["location"])
 except: pass
 try: path=urllib.unquote_plus(params["path"])
 except: pass
-try: index=urllib.unquote_plus(params["index"])
-except: pass
 
-print "Mode: "+str(mode)
-print "URL: "+str(url)
-print "Name: "+str(name)
-print "Thumb: "+str(iconimage)
-
-if mode==None or url==None or len(url)<1:
-    MAIN()    
-    VIEWSB()        
-    
-elif mode==2:
-    LISTMENU(url)
-        
-elif mode==3:
-    LISTCONTENT(url,iconimage)
-        
-elif mode==4:
-    LISTMENU2(url)
-            
-elif mode==5:
-    PLAYLINK(name,url,iconimage)
-
-elif mode==6:
-	Calendar(url)
+if mode==None or url==None or len(url)<1:MainMenu()            
+elif mode==2:VIPMenu(url)      
+elif mode==3:GetLinks(url,iconimage)      
+elif mode==4:VODMenu(url)          
+elif mode==5:PlayStream(name,url,iconimage)
+elif mode==6:Schedule(url)
         
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
