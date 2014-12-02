e8afe2e0068f497bfa1aabec37011c474a4b949d
metalkettle <james.redfern@gmail.com>
2014-12-02 23:54:41 +0000

    OpenELEC PIA 1.0.4
    Add VyprVPN support

--- plugin.program.oepia/default.py
+++ plugin.program.oepia/default.py
@@ -9,32 +9,32 @@ fanart          = ''
 icon            = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
 destfol         = '/storage/.config/vpn-config'
 ovpnpath        = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id + '/servers', ''))
-    
-if user == '' or passw == '':
-    dialog = xbmcgui.Dialog() 
-    ret = dialog.yesno('PIA for OpenELEC', 'Please enter your Private Internet Access account details','[COLOR blue]Note these are the "generated" details[/COLOR]','[COLOR blue]in your profile on http://privateinternetaccess.com[/COLOR]','Cancel','Continue')
-    if ret == 1:
-        keyb = xbmc.Keyboard('', 'Username:')
-        keyb.doModal()
-        if (keyb.isConfirmed()):
-            username = keyb.getText()
-        keyb = xbmc.Keyboard('', 'Enter Password:')
-        keyb.doModal()   
-        if (keyb.isConfirmed()):
-            password = keyb.getText()
-            selfAddon.setSetting('piauser',username)
-            selfAddon.setSetting('piapass',password)
-            user = selfAddon.getSetting('piauser')
-            passw = selfAddon.getSetting('piapass')
-    else: quit()
-                
+ovpnpathvypr        = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id + '/vyprservers', ''))
+ 
 def Index():
     addDir('[COLOR white]Setup Private Internet Access[/COLOR]','url',1,icon,'',fanart)
-    addDir('[COLOR white]Remove Private Internet Access[/COLOR]','url',2,icon,'',fanart)
+    addDir('[COLOR white]Setup VyprVPN[/COLOR]','url',5,icon,'',fanart)
+    addDir('[COLOR white]Remove VPN[/COLOR]','url',2,icon,'',fanart)
     addDir('[COLOR white]Open[/COLOR] [COLOR grey]open[/COLOR][COLOR blue]elec[/COLOR] [COLOR white]Settings[/COLOR]','url',4,icon,'',fanart)
     addDir('[COLOR white]Check My IP Location[/COLOR]','url',3,icon,'',fanart)
 
 def setup():
+    dialog = xbmcgui.Dialog() 
+    ret = dialog.yesno('Private Internet Access for OpenELEC', 'Click continue to enter your account details','','','Cancel','Continue')
+    if ret == 1:
+            keyb = xbmc.Keyboard('', 'Enter Username')
+            keyb.doModal()
+            if (keyb.isConfirmed()):
+                username = keyb.getText()
+            keyb = xbmc.Keyboard('', 'Enter Password')
+            keyb.doModal()   
+            if (keyb.isConfirmed()):
+                password = keyb.getText()
+                selfAddon.setSetting('piauser',username)
+                selfAddon.setSetting('piapass',password)
+                user = selfAddon.getSetting('piauser')
+                passw = selfAddon.getSetting('piapass')
+    
     destfol2 = destfol + '/*.*'
     files = glob.glob(destfol2)
     for f in files:
@@ -50,6 +50,37 @@ def setup():
     dialog=xbmcgui.Dialog(); dialog.ok('Setup PIA',"ALL DONE!", 'Configure from the Connections area in OpenELEC Settings')
     quit()
 
+def setupvypr():
+    dialog = xbmcgui.Dialog() 
+    ret = dialog.yesno('VyprVPN for OpenELEC', 'Click continue to enter your account details','','','Cancel','Continue')
+    if ret == 1:
+            keyb = xbmc.Keyboard('', 'Enter Username')
+            keyb.doModal()
+            if (keyb.isConfirmed()):
+                username = keyb.getText()
+            keyb = xbmc.Keyboard('', 'Enter Password')
+            keyb.doModal()   
+            if (keyb.isConfirmed()):
+                password = keyb.getText()
+                selfAddon.setSetting('piauser',username)
+                selfAddon.setSetting('piapass',password)
+                user = selfAddon.getSetting('piauser')
+                passw = selfAddon.getSetting('piapass')
+    destfol2 = destfol + '/*.*'
+    files = glob.glob(destfol2)
+    for f in files:
+        os.remove(f)
+    passpath = destfol + '/pass.txt'
+    for filename in glob.glob(os.path.join(ovpnpathvypr, '*.*')):
+        shutil.copy(filename, destfol)
+    auth = open(passpath,'a')
+    auth.write(user)
+    auth.write('\n')
+    auth.write(passw)
+    auth.close()
+    dialog=xbmcgui.Dialog(); dialog.ok('Setup VyprVPN',"ALL DONE!", 'Configure from the Connections area in OpenELEC Settings')
+    quit()
+
 def remove():
     destfol2 = destfol + '/*.*'
     files = glob.glob(destfol2)
@@ -57,7 +88,7 @@ def remove():
         os.remove(f)
     selfAddon.setSetting('piauser','')
     selfAddon.setSetting('piapass','')
-    dialog=xbmcgui.Dialog(); dialog.ok('Remove PIA',"ALL DONE!", '')
+    dialog=xbmcgui.Dialog(); dialog.ok('Remove VPN Data',"ALL DONE!", '')
     xbmc.executebuiltin("XBMC.ActivateWindow(Home)")
 
 def myip():
@@ -121,5 +152,6 @@ elif mode==1: setup()
 elif mode==2: remove()
 elif mode==3: myip()
 elif mode==4: oesettings()
+elif mode==5: setupvypr()
 
 xbmcplugin.endOfDirectory(int(sys.argv[1]))
