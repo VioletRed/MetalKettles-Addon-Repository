40b2fead467f181f8a627f2dbd9b48f986bbcb4a
metalkettle <james.redfern@gmail.com>
2015-02-04 23:47:58 +0000

    Erotik 1.0.12

--- plugin.video.erotik/default.py
+++ plugin.video.erotik/default.py
@@ -1,11 +1,12 @@
 import urllib,urllib2,re,xbmcplugin,xbmcgui,sys,xbmc,xbmcaddon,os,random,urlparse
 from t0mm0.common.addon import Addon
+from t0mm0.common.net import Net as net
 
 addon_id = 'plugin.video.erotik'
 selfAddon = xbmcaddon.Addon(id=addon_id)
 addon = Addon(addon_id, sys.argv)
 fanart = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id , 'fanart.jpg'))
-icon = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.png'))
+icon = xbmc.translatePath(os.path.join('special://home/addons/' + addon_id, 'icon.PNG'))
 base = 'http://www.ero-tik.com/index.html'
         
 def CATEGORIES():
@@ -50,48 +51,44 @@ def GETMOVIESCATS(url,name):
 
 def PLAYLINK(name,url):
         req = urllib2.Request(url)
+        print 'here'
         req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         response = urllib2.urlopen(req)
         link=response.read()
         response.close()
-        try:
-                match=re.compile('src="http://videomega.tv/validatehash.php\?hashkey=(.+?)"').findall(link)[0]
-                videomega_id_url = "http://videomega.tv/validatehash.php?hashkey="+ match
-                req = urllib2.Request(videomega_id_url,None)
-                req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:34.0) Gecko/20100101 Firefox/34.0')
-                req.add_header('Referer', url)
-                response = urllib2.urlopen(req)
-                link=response.read()
-                response.close()
-                match=re.compile('var ref="(.+?)";').findall(link)
-                vididresolved = match[0]
-        except:
-                vididresolved=re.compile('<script type="text/javascript">ref="(.+?)"').findall(link)[0]
+        match=re.compile('src="http://videomega.tv/validatehash.php\?hashkey=(.+?)"').findall(link)[0]
+        videomega_id_url = "http://videomega.tv/validatehash.php?hashkey="+ match
+        req = urllib2.Request(videomega_id_url)
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+        req.add_header('Referer', url)
+
+        response = urllib2.urlopen(req)
+        link=response.read()
+        response.close()
+        match=re.compile('var ref="(.+?)";').findall(link)
+        vididresolved = match[0]
         videomega_url = 'http://videomega.tv/?ref='+vididresolved
+
 ##RESOLVE##
         url = urlparse.urlparse(videomega_url).query
         url = urlparse.parse_qs(url)['ref'][0]
-        url = 'http://videomega.tv/iframe.php?ref=%s' % url
+        url = 'http://videomega.tv/cdn.php?ref=%s' % url
         referer = videomega_url
         req = urllib2.Request(url,None)
-        req.add_header('User-Agent', 'Mozilla/5.0 (Windows NT 6.1; rv:34.0) Gecko/20100101 Firefox/34.0')
+        req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
         req.add_header('Referer', referer)
         response = urllib2.urlopen(req)
         link=response.read()
         response.close()
         url = re.compile('document.write.unescape."(.+?)"').findall(link)[-1]
         url = urllib.unquote_plus(url)
+        print url
         stream_url = re.compile('file *: *"(.+?)"').findall(url)[0]
 ##RESOLVE##
-        playlist = xbmc.PlayList(1)
-        playlist.clear()
-        listitem = xbmcgui.ListItem(name, iconImage=icon, thumbnailImage=icon)
-        listitem.setInfo("Video", {"Title":name})
-        listitem.setProperty('mimetype', 'video/x-msvideo')
-        listitem.setProperty('IsPlayable', 'true')
-        playlist.add(stream_url,listitem)
-        xbmcPlayer = xbmc.Player(xbmc.PLAYER_CORE_AUTO)
-        xbmcPlayer.play(playlist)
+
+        liz=xbmcgui.ListItem(name, iconImage=icon,thumbnailImage=icon); liz.setInfo( type="Video", infoLabels={ "Title": name } )
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+        xbmc.Player ().play(stream_url, liz, False)
 
 def get_params():
         param=[]
