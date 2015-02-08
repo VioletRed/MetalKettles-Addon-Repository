69c27247cc4d04c68fb79def3f21ee7271467a8c
metalkettle <james.redfern@gmail.com>
2015-02-08 15:08:10 +0000

    MKSports 1.0.1

--- plugin.video.mksports/default.py
+++ plugin.video.mksports/default.py
@@ -1,1 +1,90 @@
-exec("import re;import base64");exec((lambda p,y:(lambda o,b,f:re.sub(o,b,f))(r"([0-9a-f]+)",lambda m:p(m,y),base64.b64decode("NjUgYiwxYywxNyw4Myw0ZSwxZSw0ZCwyYyw2Myw1NgoKMWQgPSc2MC42YS41MScKMTAgPSAyYy4xNSg2My41MC41NygnMzQ6Ly81OC8zZC8nICsgMWQgLCAnMTAuN2InKSkKMjMgPSAyYy4xNSg2My41MC41NygnMzQ6Ly81OC8zZC8nICsgMWQsICcyMy42MicpKQoKMzIgMmUoKToKCTFmPSc3ODovLzQ5LjZjLjdjLzY3LzY3LzVjLjdmJwoJODg9MzcoMWYpCgkyYj01Ni42ZSg4OCkKCTYxIDIyIDcxIDJiOgoJCTc5PSAyMlsnNzknXS4yNygiNGMtOCIpCgkJZT0gMjJbJ2UnXS4yNygiNGMtOCIpCgkJNWQ9MjJbJzVkJ10uMjcoIjRjLTgiKQoJCTQ0IDc5ID09ICczZScgNjQgNzkgPT0gJzc1IDNlJyA6CgkJCTVkPTVkLjM1ICgnN2QnLCcuJykKCQkJMzgoNWQsZSwyLDIzLDEwKQoJNGUuM2IoMzEoMTcuMTNbMV0pLCA0ZS4xYSkJCQkKICAKMzIgMzcoNyk6Cgk0YiA9IDFjLjU5KDcpCgk0Yi40NSgnNzAtNmInLCAnNWEvNS4wICgzOTsgODk7IDM5IDgwIDUuMTsgODQtODI7IDgxOjEuOS4wLjMpIDY4LzQ4IDVlLzMuMC4zJykKCTg4ID0gMWMuNWIoNGIpCgkyYj04OC43MigpCgk4OC42ZCgpCgkxOSAyYgoJCjMyIDUyKDYsNyk6ICAKCTNmPTU0CgkxOD0xZS4zMCg2LCAyYT0yMywxMj0yMyk7IDE4LjM2KCA1NT0iNDciLCAyMD17ICI0YSI6IDYgfSApCgkzZj00ZS5mKDQxPTMxKDE3LjEzWzFdKSw3PTcsMmY9MTgpCgkyYy42NigpLjZmKDcsIDE4LCA0MykKCTE5IDNmCgozMiAzOCg2LDcsZCw4NSwxMCw3YT0nJyk6CgkJODc9MTcuMTNbMF0rIj83PSIrYi4yNSg3KSsiJmQ9IisyOShkKSsiJjY9IitiLjI1KDYpKyImN2E9IisyOSg3YSkKCQkzZj01NAoJCTE4PTFlLjMwKDYsIDJhPSIzYS42MiIsIDEyPTg1KQoJCTE4LjM2KCA1NT0iNDciLCAyMD17ICI0YSI6IDYsICc3Myc6IDdhIH0gKQoJCTE4LjQyKCczYycsIDEwKQoJCTNmPTRlLmYoNDE9MzEoMTcuMTNbMV0pLDc9ODcsMmY9MTgsNTM9NDMpCgkJMTkgM2YKCjMyIDI0KCk6CgkJMjE9W10KCQkxYj0xNy4xM1syXQoJCTQ0IDI4KDFiKT49MjoKCQkJCTVmPTE3LjEzWzJdCgkJCQkxND01Zi4zNSgnPycsJycpCgkJCQk0NCAoNWZbMjgoNWYpLTFdPT0nLycpOgoJCQkJCQk1Zj01ZlswOjI4KDVmKS0yXQoJCQkJYT0xNC40NignJicpCgkJCQkyMT17fQoJCQkJNjEgODYgNzEgNjkoMjgoYSkpOgoJCQkJCQk0PXt9CgkJCQkJCTQ9YVs4Nl0uNDYoJz0nKQoJCQkJCQk0NCAoMjgoNCkpPT0yOgoJCQkJCQkJCTIxWzRbMF1dPTRbMV0JCgkJMTkgMjEKCQkgICAKNWY9MjQoKQo3PTExCjY9MTEKZD0xMQo4NT0xMQo3YT0xMQoKNDA6Nz1iLmMoNWZbIjciXSkKMTY6MmQKNDA6Nj1iLmMoNWZbIjYiXSkKMTY6MmQKNDA6ZD0zMSg1ZlsiZCJdKQoxNjoyZAo0MDo4NT1iLmMoNWZbIjg1Il0pCjE2OjJkCjI2ICI3NjogIisyOShkKTsyNiAiN2U6ICIrMjkoNyk7MjYgIjc3OiAiKzI5KDYpOzI2ICI0ZjogIisyOSg4NSkKCjQ0IGQ9PTExIDY0IDc9PTExIDY0IDI4KDcpPDE6MmUoKQo3NCBkPT0yOjUyKDYsNykKCjRlLjMzKDMxKDE3LjEzWzFdKSk=")))(lambda a,b:b[int("0x"+a.group(1),16)],"0|1|2|3|splitparams|5|name|url|8|9|pairsofparams|urllib|unquote_plus|mode|channelLink|addDirectoryItem|fanart|None|thumbnailImage|argv|cleanedparams|translatePath|except|sys|liz|return|SORT_METHOD_VIDEO_TITLE|paramstring|urllib2|AddonID|xbmcgui|channelurl|infoLabels|param|field|icon|get_params|quote_plus|print|encode|len|str|iconImage|link|xbmc|pass|Channels|listitem|ListItem|int|def|endOfDirectory|special|replace|setInfo|Get_url|addLink|Windows|DefaultFolder|addSortMethod|fanart_image|addons|Sports|ok|try|handle|setProperty|False|if|add_header|split|Video|2008092417|worldtvpro|Title|req|utf|xbmcaddon|xbmcplugin|IconImage|path|mksports|Play|isFolder|True|type|json|join|home|Request|Mozilla|urlopen|jklmnop|channelName|Firefox|params|plugin|for|png|os|or|import|Player|cms|Gecko|range|video|Agent|zapto|close|loads|play|User|in|read|plot|elif|Bein|Mode|Name|http|categoryName|description|jpg|org|3pm|URL|php|NT|rv|GB|re|en|iconimage|i|u|response|U".split("|")))
+import urllib,urllib2,sys,re,xbmcplugin,xbmcgui,xbmcaddon,xbmc,os,json
+
+AddonID ='plugin.video.mksports'
+fanart = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID , 'fanart.jpg'))
+icon = xbmc.translatePath(os.path.join('special://home/addons/' + AddonID, 'icon.png'))
+    
+def Collection1():
+    channelurl='http://pushpakglobal.dynns.com/cms/cms/jGlobal.php'
+    response=Get_url(channelurl)
+    link=json.loads(response)
+    for field in link:
+        categoryName= field['categoryName'].encode("utf-8")
+        channelLink= field['channelLink'].encode("utf-8")
+        channelName=field['channelName'].encode("utf-8")
+        if categoryName == 'Sports' or categoryName == 'Bein Sports' or categoryName == 'OTE Sports' or categoryName == 'Football' or categoryName == 'Cricket World Cup 2015':
+            if not "3pm" in channelName:
+                addLink(channelName,channelLink,100,icon,fanart)
+    xbmcplugin.addSortMethod(int(sys.argv[1]), xbmcplugin.SORT_METHOD_VIDEO_TITLE)
+
+def Get_url(url):
+    req = urllib2.Request(url)
+    req.add_header('User-Agent', 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-GB; rv:1.9.0.3) Gecko/2008092417 Firefox/3.0.3')
+    response = urllib2.urlopen(req)
+    link=response.read()
+    response.close()
+    return link
+    
+def Play(name,url):  
+    ok=True
+    liz=xbmcgui.ListItem(name, iconImage=icon,thumbnailImage=icon); liz.setInfo( type="Video", infoLabels={ "Title": name } )
+    ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=url,listitem=liz)
+    xbmc.Player().play(url, liz, False)
+    return ok
+
+def addLink(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=False)
+        return ok
+
+def addDir(name,url,mode,iconimage,fanart,description=''):
+        u=sys.argv[0]+"?url="+urllib.quote_plus(url)+"&mode="+str(mode)+"&name="+urllib.quote_plus(name)+"&description="+str(description)
+        ok=True
+        liz=xbmcgui.ListItem(name, iconImage="DefaultFolder.png", thumbnailImage=iconimage)
+        liz.setInfo( type="Video", infoLabels={ "Title": name, 'plot': description } )
+        liz.setProperty('fanart_image', fanart)
+        ok=xbmcplugin.addDirectoryItem(handle=int(sys.argv[1]),url=u,listitem=liz,isFolder=True)
+        return ok
+
+def get_params():
+        param=[]
+        paramstring=sys.argv[2]
+        if len(paramstring)>=2:
+                params=sys.argv[2]
+                cleanedparams=params.replace('?','')
+                if (params[len(params)-1]=='/'):
+                        params=params[0:len(params)-2]
+                pairsofparams=cleanedparams.split('&')
+                param={}
+                for i in range(len(pairsofparams)):
+                        splitparams={}
+                        splitparams=pairsofparams[i].split('=')
+                        if (len(splitparams))==2:
+                                param[splitparams[0]]=splitparams[1]    
+        return param
+           
+params=get_params()
+url=None
+name=None
+mode=None
+iconimage=None
+description=None
+
+try:url=urllib.unquote_plus(params["url"])
+except:pass
+try:name=urllib.unquote_plus(params["name"])
+except:pass
+try:mode=int(params["mode"])
+except:pass
+try:iconimage=urllib.unquote_plus(params["iconimage"])
+except:pass
+print "Mode: "+str(mode);print "URL: "+str(url);print "Name: "+str(name);print "IconImage: "+str(iconimage)
+
+if mode==None or url==None or len(url)<1:Collection1()
+elif mode==100:Play(name,url)
+
+xbmcplugin.endOfDirectory(int(sys.argv[1]))
