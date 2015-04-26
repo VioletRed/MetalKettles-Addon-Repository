9a0dc3a5bb2cce6c8d96bdb9b3faf68c4dc1362a
metalkettle <james.redfern@gmail.com>
2015-04-26 19:45:12 +0100

    Stalker 1.0.5

---
+++ plugin.video.stalker/addon.py
@@ -0,0 +1,284 @@
+﻿import sys
+import os
+import json
+import urllib
+import urlparse
+import xbmcaddon
+import xbmcgui
+import xbmcplugin
+import load_channels
+import hashlib
+
+addon       = xbmcaddon.Addon()
+addonname   = addon.getAddonInfo('name')
+addondir    = xbmc.translatePath( addon.getAddonInfo('profile') ) 
+
+base_url = sys.argv[0]
+addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+
+
+#xbmcgui.Dialog().ok(addonname, 'aaa')
+
+xbmcplugin.setContent(addon_handle, 'movies')
+
+
+mode = args.get('mode', None)
+
+portal_name_1 = args.get('portal_url_1', None)
+portal_url_1 = args.get('portal_url_1', None)
+
+portal_name_2 = args.get('portal_url_1', None)
+portal_url_2 = args.get('portal_url_1', None)
+
+portal_name_3 = args.get('portal_url_3', None)
+portal_url_3 = args.get('portal_url_3', None)
+
+#xbmcgui.Dialog().ok(addonname, 'aaa')
+
+if portal_url_1 is None:
+	parental = xbmcplugin.getSetting(int(sys.argv[1]), "parental");
+	password = xbmcplugin.getSetting(int(sys.argv[1]), "password");
+	
+	portal_name_1 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_name_1");
+	portal_url_1 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_url_1");
+	
+	portal_name_2 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_name_2");
+	portal_url_2 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_url_2");
+	
+	portal_name_3 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_name_3");
+	portal_url_3 = xbmcplugin.getSetting(int(sys.argv[1]), "portal_url_3");
+else:
+	parental = parental[0];
+	password = password[0];
+	portal_name_1 = portal_name_1[0];
+	portal_url_1 = portal_url_1[0];
+	
+	portal_name_2 = portal_name_2[0];
+	portal_url_2 = portal_url_2[0];
+	
+	portal_name_3 = portal_name_3[0];
+	portal_url_3 = portal_url_3[0];
+	
+
+
+def addPortal(portal_name, portal_url):
+	if portal_url != '':
+		
+		url = build_url({
+			'mode': 'genres', 
+			'stalker_url' : portal_url, 
+			'title' : portal_name
+			});
+			
+			
+		cmd = 'XBMC.RunPlugin(' + base_url + '?mode=cache&stalker_url=' + portal_url + ')';		
+		li = xbmcgui.ListItem(portal_name, iconImage='DefaultProgram.png')
+		li.addContextMenuItems([ ('Clear Cache', cmd) ]);
+
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True);
+	
+	
+def build_url(query):
+	return base_url + '?' + urllib.urlencode(query)
+
+
+def homeLevel():
+	addPortal(portal_name_1,portal_url_1);
+	addPortal(portal_name_2,portal_url_2);
+	addPortal(portal_name_3,portal_url_3);
+	xbmcplugin.endOfDirectory(addon_handle);
+
+def genreLevel():
+	stalker_url = args.get('stalker_url', None)
+	stalker_url = stalker_url[0];
+	
+	data = load_channels.getGenres(stalker_url, addondir);
+	data = data['genres'];
+	
+
+		
+	url = build_url({
+		'mode': 'vod', 
+		'stalker_url' : stalker_url
+	});
+			
+	li = xbmcgui.ListItem('VoD', iconImage='DefaultVideo.png')
+	xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True);
+	
+	
+	for i in data:
+
+		id 		= i["id"];
+		title 	= i["title"];
+		
+		#xbmcgui.Dialog().ok(addonname, title);
+		
+		url = build_url({
+			'mode': 'channels', 
+			'stalker_url' : stalker_url, 
+			'genre_id': id, 
+			'genre_name': title.title()
+			});
+			
+		if id == '10':
+			iconImage = 'OverlayLocked.png';
+		else:
+			iconImage = 'DefaultVideo.png';
+			
+		li = xbmcgui.ListItem(title.title(), iconImage=iconImage)
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li, isFolder=True);
+		
+
+	xbmcplugin.endOfDirectory(addon_handle);
+
+def vodLevel():
+	stalker_url = args.get('stalker_url', None);
+	stalker_url = stalker_url[0];
+	
+	
+	data = load_channels.getVoD(stalker_url, addondir);
+	data = data['vod'];
+	
+		
+	for i in data:
+		name 	= i["name"];
+		cmd 	= i["cmd"];
+		logo 	= i["logo"];
+		
+		
+		if logo != '':
+			logo_url = stalker_url + logo;
+		else:
+			logo_url = 'DefaultVideo.png';
+				
+				
+		url = build_url({
+				'mode': 'play', 
+				'stalker_url' : stalker_url, 
+				'cmd': cmd, 
+				'tmp' : '0', 
+				'title' : name.encode("utf-8"),
+				'genre_name' : 'VoD',
+				'logo_url' : logo_url
+				});
+			
+
+		li = xbmcgui.ListItem(name, iconImage=logo_url, thumbnailImage=logo_url)
+		li.setInfo(type='Video', infoLabels={ "Title": name })
+		#li.setProperty('IsPlayable', 'true')
+
+		xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+	
+	xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_UNSORTED);
+	xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_TITLE);
+	xbmcplugin.endOfDirectory(addon_handle);
+
+def channelLevel():
+	stop=False;
+	
+	stalker_url = args.get('stalker_url', None);
+	stalker_url = stalker_url[0];
+	
+	genre_id_main = args.get('genre_id', None);
+	genre_id_main = genre_id_main[0];
+	
+	data = load_channels.getAllChannels(stalker_url, addondir);
+	data = data['channels'];
+	
+	genre_name 	= args.get('genre_name', None);
+	
+	
+	if genre_id_main == '10' and parental == 'true':
+		result = xbmcgui.Dialog().input('Parental', hashlib.md5(password.encode('utf-8')).hexdigest(), type=xbmcgui.INPUT_PASSWORD, option=xbmcgui.PASSWORD_VERIFY);
+		if result == '':
+			stop = True;
+
+	if stop == False:
+		for i in data:
+			name 	= i["name"];
+			cmd 	= i["cmd"];
+			tmp 	= i["tmp"];
+			number 	= i["number"];
+			genre_id 	= i["genre_id"];
+			logo 	= i["logo"];
+		
+			if genre_id_main == '*' and genre_id == '10':
+				continue;
+		
+			if genre_id_main == genre_id or genre_id_main == '*':
+		
+				if logo != '':
+					logo_url = stalker_url + '/stalker_portal/misc/logos/320/' + logo;
+				else:
+					logo_url = 'DefaultVideo.png';
+				
+				
+				url = build_url({
+					'mode': 'play', 
+					'stalker_url' : stalker_url, 
+					'cmd': cmd, 
+					'tmp' : tmp, 
+					'title' : name.encode("utf-8"),
+					'genre_name' : genre_name,
+					'logo_url' : logo_url
+					});
+			
+
+				li = xbmcgui.ListItem(name, iconImage=logo_url, thumbnailImage=logo_url);
+				li.setInfo(type='Video', infoLabels={ "title": name });
+
+				xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li);
+		
+		xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_UNSORTED);
+		xbmcplugin.addSortMethod(addon_handle, xbmcplugin.SORT_METHOD_TITLE);
+		
+		
+		xbmcplugin.endOfDirectory(addon_handle);
+
+def playLevel():
+	stalker_url = args.get('stalker_url', None)
+	stalker_url = stalker_url[0];
+	
+	title 	= args['title'][0];
+	cmd 	= args['cmd'][0];
+	tmp 	= args['tmp'][0];
+	genre_name 	= args['genre_name'][0];
+	logo_url 	= args['logo_url'][0];
+	
+	if genre_name!='VoD':
+		url = load_channels.retriveUrl(stalker_url, cmd, tmp);
+	else:
+		url = load_channels.retriveVoD(stalker_url, cmd);
+	
+	li = xbmcgui.ListItem(title, iconImage=logo_url);
+	li.setInfo('video', {'Title': title, 'Genre': genre_name});
+	xbmc.Player().play(item=url, listitem=li);
+
+
+if mode is None:
+	homeLevel();
+
+elif mode[0] == 'cache':	
+	stalker_url = args.get('stalker_url', None);
+	stalker_url = stalker_url[0];	
+	load_channels.clearCache(stalker_url, addondir);
+
+elif mode[0] == 'genres':
+	genreLevel();
+		
+elif mode[0] == 'vod':
+	vodLevel();
+
+	
+elif mode[0] == 'channels':
+	channelLevel();
+	
+elif mode[0] == 'play':
+	playLevel();
+
+
+
+
+
+	
---
+++ plugin.video.stalker/load_channels.py
@@ -0,0 +1,392 @@
+import sys
+import urllib
+import urllib2
+import json
+import os
+import urlparse
+import re, uuid
+from time import time
+from datetime import datetime
+import math
+
+key = ''
+mac = ':'.join(re.findall('..', '%012x' % uuid.getnode()))
+
+
+def handshake(url):
+	global key;
+	
+	info = retrieveData(url, values = {
+		'type' : 'stb', 
+		'action' : 'handshake',
+		'JsHttpRequest' : '1-xml'})
+
+	key = info['js']['token']
+	
+	#getProfile(url);
+	
+	#print 'key: ' + key
+
+def getProfile(url):
+	global mac, key;
+		
+	sn = "".join(re.findall("[0-9]+", mac));
+	sn += sn + sn;
+	sn = sn[:13];
+
+	info = retrieveData(url, values = {
+		'type' : 'stb', 
+		'action' : 'get_profile',
+		'hd' : '1',
+		'ver' : 'ImageDescription:%200.2.18-r10-pub-250;%20ImageDate:%2014%20Jan%202015%2013:18:32%20GMT%200200;%20PORTAL%20version:%20Latest;%20API%20Version:%20JS%20API%20version:%20328;%20STB%20API%20version:%20134;%20Player%20Engine%20version:%200x560',
+		'num_banks' : '1',
+		'sn' : sn,
+		'stb_type' : 'MAG250',
+		'image_version' : '218',
+		'auth_second_step' : '0',
+		'hw_version' : '1.7-BD-00',
+		'not_valid_token' : '0',
+		'JsHttpRequest' : '1-xml'})
+
+
+def retrieveData(url, values ):
+	global key, mac;
+	
+	
+	url += '/stalker_portal'
+	load = '/server/load.php'
+	refer = '/c/'
+	timezone = 'Europe%2FKiev';
+
+	user_agent 	= 'Mozilla/5.0 (QtEmbedded; U; Linux; C) AppleWebKit/533.3 (KHTML, like Gecko) MAG200 stbapp ver: 2 rev: 250 Safari/533.3';
+	
+	if key != '':
+		#print 'Authorization Bearer ' + key;
+		
+		headers 	= { 
+			'User-Agent' : user_agent, 
+			'Cookie' : 'mac=' + mac.replace(':', '%3') + '; stb_lang=en; timezone=' + timezone,
+			'Referer' : url + refer,
+			'Accept' : '*/*',
+			'X-User-Agent' : 'Model: MAG250; Link: WiFi',
+			'Authorization' : 'Bearer ' + key };
+	
+	else:
+		headers 	= { 
+			'User-Agent' : user_agent, 
+			'Cookie' : 'mac=' + mac+ '; stb_lang=en; timezone=' + timezone,
+			'Referer' : url + refer,
+			'Accept' : '*/*',
+			'X-User-Agent' : 'Model: MAG250; Link: WiFi' };
+		
+
+
+	data = urllib.urlencode(values);
+	req = urllib2.Request(url + load, data, headers);
+	data = urllib2.urlopen(req).read().decode("utf-8");
+	info = json.loads(data)
+
+	return info;
+
+
+def getGenres(url, path):	
+	global key;
+	now = time();
+	portalurl = "_".join(re.findall("[a-zA-Z0-9]+", url));
+	portalurl = path + '/' + portalurl + '-genres';
+	
+	
+	if os.path.exists(portalurl):
+		#check last time
+		with open(portalurl) as data_file: data = json.load(data_file);
+	
+		time_init = float(data['time']);
+		
+	
+		# update 12h
+		if ((now - time_init) / 3600) < 12:
+			return data;
+	
+	handshake(url);
+	
+	info = retrieveData(url, values = {
+		'type' : 'itv', 
+		'action' : 'get_genres',
+		'JsHttpRequest' : '1-xml'})
+		
+	
+	results = info['js']
+	
+	data = '{ "time" : "' + str(now) + '", "genres" : [ \n'
+
+	for i in results:
+		alias 	= i["alias"]
+		id 		= i["id"]
+		title 	= i['title']
+		
+		data += '{"id":"'+ id +'", "alias":"'+ alias +'", "title":"'+ title +'"}, \n'
+
+	
+	data = data[:-3] + '\n]}'
+
+	with open(portalurl, 'w') as f: f.write(data.encode('utf-8'));
+	
+	return json.loads(data.encode('utf-8'));
+	
+def getVoD(url, path):	
+	global key;
+	now = time();
+	portalurl = "_".join(re.findall("[a-zA-Z0-9]+", url));
+	portalurl = path + '/' + portalurl + '-vod';
+	
+	
+	if os.path.exists(portalurl):
+		#check last time
+		with open(portalurl) as data_file: data = json.load(data_file);
+	
+		time_init = float(data['time']);
+		
+	
+		# update 12h
+		if ((now - time_init) / 3600) < 12:
+			return data;
+	
+	handshake(url);
+	
+	data = '{ "time" : "' + str(now) + '", "vod" : [ \n'
+	
+	page = 1;
+	pages = 0;
+	total_items = 1.0;
+	max_page_items = 1.0;
+	
+	while True:
+		info = retrieveData(url, values = {
+			'type' : 'vod', 
+			'action' : 'get_ordered_list',
+			'sortby' : 'added',
+			'not_ended' : '0',
+			'p' : page,
+			'fav' : '0',
+			'JsHttpRequest' : '1-xml'})
+		
+		total_items = float(info['js']['total_items']);
+		max_page_items = float(info['js']['max_page_items']);
+		pages = math.ceil(total_items/max_page_items);
+		
+		results = info['js']['data']
+
+
+		for i in results:
+			name 	= i["name"]
+			cmd 	= i['cmd']
+			logo 	= i["screenshot_uri"]
+		
+			data += '{"name":"'+ name +'", "cmd":"'+ cmd +'", "logo":"'+ logo +'"}, \n'
+
+		page += 1;
+		if page > pages or page == 10:
+			break;
+
+	data = data[:-3] + '\n]}'
+
+	with open(portalurl, 'w') as f: f.write(data.encode('utf-8'));
+	
+	return json.loads(data.encode('utf-8'));
+
+def getAllChannels(url, path):
+
+	now = time();
+	
+	portalurl = "_".join(re.findall("[a-zA-Z0-9]+", url));
+	portalurl = path + '/' + portalurl
+	
+
+	if os.path.exists(portalurl):
+		#check last time
+		with open(portalurl) as data_file: data = json.load(data_file);
+	
+		time_init = float(data['time']);
+	
+		# update 12h
+		if ((now - time_init) / 3600) < 12:
+			return data;
+	
+	handshake(url);
+	
+	info = retrieveData(url, values = {
+		'type' : 'itv', 
+		'action' : 'get_all_channels',
+		'JsHttpRequest' : '1-xml'})
+	
+	
+	results = info['js']['data']
+
+	data = '{ "time" : "' + str(now) + '", "channels" : [ \n'
+
+	for i in results:
+		number 	= i["number"]
+		name 	= i["name"]
+		cmd 	= i['cmd']
+		logo 	= i["logo"]
+		tmp 	= i["use_http_tmp_link"]
+		genre_id 	= i["tv_genre_id"];
+		
+		data += '{"number":"'+ number +'", "name":"'+ name +'", "cmd":"'+ cmd +'", "logo":"'+ logo +'", "tmp":"'+ str(tmp) +'", "genre_id":"'+ str(genre_id) +'"}, \n'
+
+
+	page = 1;
+	pages = 0;
+	total_items = 1.0;
+	max_page_items = 1.0;
+
+	while True:
+		# retrieve adults
+		info = retrieveData(url, values = {
+			'type' : 'itv', 
+			'action' : 'get_ordered_list',
+			'genre' : '10',
+			'p' : page,
+			'fav' : '0',
+			'JsHttpRequest' : '1-xml'})
+	
+		total_items = float(info['js']['total_items']);
+		max_page_items = float(info['js']['max_page_items']);
+		pages = math.ceil(total_items/max_page_items);
+	
+		results = info['js']['data']
+
+		for i in results:
+			number 	= i["number"]
+			name 	= i["name"]
+			cmd 	= i['cmd']
+			logo 	= i["logo"]
+			tmp 	= i["use_http_tmp_link"]
+			genre_id 	= i["tv_genre_id"];
+		
+			data += '{"number":"'+ number +'", "name":"'+ name +'", "cmd":"'+ cmd +'", "logo":"'+ logo +'", "tmp":"'+ str(tmp) +'", "genre_id":"'+ str(genre_id) +'"}, \n'
+
+		page += 1;
+		if page > pages:
+			break;
+	
+	data = data[:-3] + '\n]}'
+	
+
+	with open(portalurl, 'w') as f: f.write(data.encode('utf-8'));
+	
+	return json.loads(data.encode('utf-8'));
+
+def retriveUrl(url, channel, tmp):
+	
+	cmd = channel;
+	
+	if tmp != "0":
+		info = retrieveData(url, values = {
+			'type' : 'itv', 
+			'action' : 'create_link', 
+			'cmd' : channel,
+			'forced_storage' : 'undefined',
+			'disable_ad' : '0',
+			'JsHttpRequest' : '1-xml'});
+		cmd = info['js']['cmd'];
+		
+	s = cmd.split(' ');
+			
+	url = s[0];
+	
+	if len(s)>1:
+		url = s[1];
+		
+	#print url;
+
+	if tmp != "0":
+		# RETRIEVE THE 1 EXTM3U
+		request = urllib2.Request(url)
+		request.get_method = lambda : 'HEAD'
+		response  = urllib2.urlopen(request);
+		data = response.read().decode("utf-8");
+		data = data.splitlines();
+		data = data[len(data) - 1];
+	
+		# RETRIEVE THE 2 EXTM3U
+		url = response.geturl().split('?')[0];
+		url_base = url[: -(len(url) - url.rfind('/'))]
+		return url_base + '/' + data;
+	else:
+		return url;
+
+
+def retriveVoD(url, video):
+	
+	s = video.split(' ');
+	url = s[0];
+	if len(s)>1:
+		url = s[1];
+
+	
+	url = url.replace('TOMTOM:', 'http://');
+	
+	#print url;
+
+	# RETRIEVE THE 1 EXTM3U
+	request = urllib2.Request(url)
+	response  = urllib2.urlopen(request);
+	url = response.geturl();
+
+
+	# RETRIEVE THE 1 EXTM3U
+	request = urllib2.Request(url)
+	#request.get_method = lambda : 'HEAD'
+	response  = urllib2.urlopen(request);
+	data = response.read().decode("utf-8");
+	data = data.splitlines();
+	data = data[len(data) - 1];
+	
+	# RETRIEVE THE 2 EXTM3U
+	url = response.geturl().split('?')[0];
+	url_base = url[: -(len(url) - url.rfind('/'))]
+	return url_base + '/' + data;
+
+def clearCache(url, path):
+	
+	portalurl = "_".join(re.findall("[a-zA-Z0-9]+", url));
+	
+	for root, dirs, files in os.walk(path):
+		for file in files:
+			if file.startswith(portalurl):
+				os.remove(root + '/' + file);
+	
+
+def main(argv):
+
+      if argv[0] == 'load':
+      	getAllChannels(argv[1], argv[2]);
+      	
+      elif argv[0] == 'genres':
+      	getGenres(argv[1], argv[2]);
+      	
+      elif argv[0] == 'vod':
+      	getVoD(argv[1], argv[2]);
+      	
+      elif argv[0] == 'channel':
+      	url = retriveUrl(argv[1], argv[2], argv[3]);
+      	print url
+	
+      elif argv[0] == 'vod_url':
+      	url = retriveVoD(argv[1], argv[2]);
+      	print url
+      	os.system('/Applications/VLC.app/Contents/MacOS/VLC ' + url)
+      	
+      elif argv[0] == 'cache':
+      	clearCache(argv[1], argv[2]);
+      	
+      elif argv[0] == 'profile':
+      	handshake(argv[1]);
+
+
+
+if __name__ == "__main__":
+   main(sys.argv[1:])
+
+
